03eaa49a8dc0ce71503c84995fba81102f009f34
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-02-13 17:01:31 +0000

    complete all steps 1 - 5

--- run_analysis.R
+++ run_analysis.R
@@ -21,6 +21,7 @@ names(df_xtest) <- col_names
 #load the test activity data
 df_ytest <- read.table("./data/test/y_test.txt")
 names(df_ytest) <- c("Activity")
+
 #load test subject data
 df_sub_test <- read.table("./data/test/subject_test.txt")
 names(df_sub_test) <- c("Subject")
@@ -54,8 +55,7 @@ rm(col_names,
 
 
 # 2. extract mean and std for each measurement
-
-df_test_train <- df_test_train[,grep("Subject|Activity|mean|std", 
+df_test_train <- df_test_train[,grep("Subject|Activity|[mM]ean|std", 
                                      names(df_test_train))]
 
 # 3. change activity name to something descriptive
@@ -66,14 +66,22 @@ act_labels <- read.table("./data/activity_labels.txt")[[2]]
 # replace data with activity label
 df_test_train <- mutate(df_test_train, Activity = act_labels[Activity])
 
+#clean up environment
+rm(act_labels)
 
-# 4. use descriptive variable names - clean up names a bit?
-
-
+# 4. use descriptive variable names - 
+# variable names already added in step 1
 
 
 # 5. from data in step 4, create new data set ave for each act and each subject
 
+df_summary <- df_test_train %>% 
+        pivot_longer(cols = 3:81) %>% # pivot longer for one variable per row
+        group_by(Subject, Activity, name) %>% # add  grouping 
+        summarise(mean = mean(value)) %>% # summarise and calculate mean 
+        pivot_wider(names_from = name, values_from = mean) #back to wide data
+
+# save out summary DF
 
 # documentation
 
