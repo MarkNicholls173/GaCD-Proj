0fa8490d44cb82ad43dede15c6c417e289b10144
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-02-14 15:59:25 +0000

    add folder with data and update folder name in script

--- run_analysis.R
+++ run_analysis.R
@@ -1,48 +1,51 @@
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each 
-#    measurement. 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
-# 5. From the data set in step 4, creates a second, independent tidy data set 
-#    with the average of each variable for each activity and each subject.
+# run_analysis file created for the Getting and Cleaning Data course project
+# assumes data in a folder called "UCI HAR Dataset" in the current working
+# directory
 
-library(dplyr)
+# the Review criteria, are numbered and included as comments in this code 
+# before the steps that fulfill them
+
+#load required packages
+
+library(dplyr) # needed for the merge command
+library(tidyr) # needed for pivot_longer and pivot_wider commends
 
 
-# 1. merge the data sets
+# 1. Merge the training and the test sets to create one data set.
+# and
+# 4. Appropriately label the data set with descriptive variable names. 
 
-#get variable names, common to both test and train 
-col_names <- read.table("./data/features.txt")[[2]]
+#get variable names, common to both test and train
+col_names <- read.table("./UCI HAR Dataset/features.txt")[[2]]
 
 #load test data
-df_xtest <- read.table("./data/test/X_test.txt")
-names(df_xtest) <- col_names
+df_xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
+names(df_xtest) <- col_names # set the variable names
 
 #load the test activity data
-df_ytest <- read.table("./data/test/y_test.txt")
-names(df_ytest) <- c("Activity")
+df_ytest <- read.table("./UCI HAR Dataset/test/y_test.txt")
+names(df_ytest) <- c("Activity") # set the variable name
 
 #load test subject data
-df_sub_test <- read.table("./data/test/subject_test.txt")
-names(df_sub_test) <- c("Subject")
+df_sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+names(df_sub_test) <- c("Subject") # set the variable name
 
-#combine the 3 
+#combine the test data
 df_test_com <- cbind(df_sub_test, df_ytest, df_xtest)
 
-
 #load train data
-df_xtrain <- read.table("./data/train/X_train.txt")
-names(df_xtrain) <- col_names
+df_xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
+names(df_xtrain) <- col_names # set the variable names
 
 #load the train activity data
-df_ytrain <- read.table("./data/train/y_train.txt")
-names(df_ytrain) <- c("Activity")
+df_ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
+names(df_ytrain) <- c("Activity") # set the variable name
 
 #load train subject data
-df_sub_train <- read.table("./data/train/subject_train.txt")
-names(df_sub_train) <- c("Subject")
+df_sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+names(df_sub_train) <- c("Subject") # set the variable name
 
-#combine the 3
+#combine the train data
 df_train_com <- cbind(df_sub_train, df_ytrain, df_xtrain)
 
 #combine test and train data
@@ -54,14 +57,14 @@ rm(col_names,
    df_xtrain, df_ytrain, df_train_com, df_sub_train)
 
 
-# 2. extract mean and std for each measurement
+# 2. Extracts only the measurements on the mean and standard deviation for each 
+#    measurement.
 df_test_train <- df_test_train[,grep("Subject|Activity|[mM]ean|std", 
                                      names(df_test_train))]
 
-# 3. change activity name to something descriptive
-
+# 3. Use descriptive activity names to name the activities in the data set
 # load the activity labels
-act_labels <- read.table("./data/activity_labels.txt")[[2]]
+act_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[[2]]
 
 # replace data with activity label
 df_test_train <- mutate(df_test_train, Activity = act_labels[Activity])
@@ -69,11 +72,9 @@ df_test_train <- mutate(df_test_train, Activity = act_labels[Activity])
 #clean up environment
 rm(act_labels)
 
-# 4. use descriptive variable names - 
-# variable names already added in step 1
 
-
-# 5. from data in step 4, create new data set ave for each act and each subject
+# 5. From the data set in step 4, create a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
 
 df_summary <- df_test_train %>% 
         pivot_longer(cols = 3:81) %>% # pivot longer for one variable per row
@@ -82,6 +83,8 @@ df_summary <- df_test_train %>%
         pivot_wider(names_from = name, values_from = mean) #back to wide data
 
 # save out summary DF
+write.table(df_summary, file = "summary_table.txt", row.names = FALSE)
+
 
 # documentation
 
